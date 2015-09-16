3fe7e7f39b4b7d4e4a8e12e056ebf43589c42f7e
Nickolay <lukaville@gmail.com>
2015-09-16 23:16:42 +0300

    fix rtl margin + add more languages

--- app/src/main/java/com/nbsp/translator/api/Languages.java
+++ app/src/main/java/com/nbsp/translator/api/Languages.java
@@ -11,7 +11,13 @@ import java.util.List;
  */
 
 public class Languages {
-    private static final String[] LANGUAGES = {"ru", "en", "be", "da", "de", "fr", "ja", "uk", "zh", "az"};
+    private static final String[] LANGUAGES = {"ru", "en", "af", "ar", "az", "be", "bg",
+            "bs", "ca", "cs", "cy", "da", "de", "el", "es", "et", "eu", "fa", "fi", "fr",
+            "ga", "gl", "he", "hr", "ht", "hu", "hy", "id", "is", "it", "ja", "ka", "kk",
+            "ko", "ky", "la", "lt", "lv", "mg", "mk", "mn", "ms", "mt", "nl", "no", "pl",
+            "pt", "ro", "sk", "sl", "sq", "sr", "sv", "sw", "tg", "th", "tl", "tr", "tt",
+            "uk", "uz", "vi", "zh"};
+
     private static Languages ourInstance = new Languages();
     private List<Language> mLanguages;
     private TranslationDirection mTranslationDirection;
